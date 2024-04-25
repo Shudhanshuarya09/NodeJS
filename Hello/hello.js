a30fb5dcdaafd6fc8d62afd713021d6807cbde9f
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-04-25 17:33:47 +0530

    Modules in nodeJS

--- Hello/hello.js
+++ Hello/hello.js
@@ -1,1 +1,10 @@
-console.log("Hello im shudhanshu arya");
+// console.log("Hello im shudhanshu arya");
+
+// const math = require("./math");
+// console.log("Math value is ", math.sub(4,2));
+
+//we can also write the above code into thus form
+
+const {add , sub} = require("./math");
+
+console.log("Math value is ", sub(4,2));
---
+++ Hello/math.js
@@ -0,0 +1,14 @@
+function add(a,b){
+  return a + b;
+}
+
+function sub(a,b){
+  return a - b;
+}
+
+module.exports = {
+    add,
+    sub,
+};
+
+//exports.add = (a,b) => a+b;     **we can aslo exports like this by using the arrow function
