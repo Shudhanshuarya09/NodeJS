01418e990e64d78d6a97fc1227bc09ee1863dbeb
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-04-26 18:59:38 +0530

    Append concept clear

---
+++ Hello/file.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+//[To create/write a File]
+// 
+// fs.writeFileSync('./test.txt', "Hello");       //For synchronous 
+
+// 
+// fs.writeFile("./test.txt", "Hello", (err) => {});     //For Asynchronus
+
+// //[To read a File]
+
+// const result = fs.readFileSync("./contact.txt", "utf-8");   //synchronus
+// console.log(result);
+
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {    //For Asynchronus
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log(result);
+//   }
+// });   
+// console.log(result);
+
+//[To append a File]
+
+fs.appendFileSync("./test.txt", ' ${Date.now()} Hey There\n' );
