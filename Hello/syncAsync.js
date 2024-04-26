631575a0a1575a40bd2a6418e360161e22e71174
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-04-26 19:32:21 +0530

    Non blocking conept cleared

--- Hello/syncAsync.js
+++ Hello/syncAsync.js
@@ -9,4 +9,15 @@ const result = fs.readFileSync("./contact.txt", "utf-8");
 console.log("result");
 
 console.log("2");
-console.log("3");
+console.log("3");     //here output came 1,result,2,3  result wala processs kiya hoga tbtk 2,3 wait kiya hoga jbki wo chutki mai excute hojata as compared to result but synchronus hai to thread block hogya so wait kiya or ye output diya.
+
+
+//Non-Blocking Operation - Asynchronus code concept
+console.log("1");
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+  console.log(result);
+});
+
+
+console.log("2");
+console.log("3");     //so here the output is 1,2,3,then result araam se kuki block nhi kiya thread
